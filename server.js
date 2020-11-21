e1e7e1349f3c3e8c9d9dfca5f42d958500018364
Gene <gjy80230@foxmail.com>
2020-11-21 23:01:36 +0800

    修改server文件

--- server.js
+++ server.js
@@ -22,22 +22,11 @@ var server = http.createServer(function(request, response){
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
   if(path === '/'){
-    response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <link rel="stylesheet" href="/x">
-      </head>
-      <body>
-        <h1>标题</h1>
-      <script src="/y"></script>
-      </body>
-      </thml>
-    `)
-    response.end()
-  }else if(path === '/x'){
+       response.statusCode = 200
+       response.setHeader('Content-Type', 'text/html;charset=utf-8')
+       response.write(`二哈\n`)
+       response.end()  
+    }else if(path === '/x'){
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
     response.write(`body{color:red;}`)
@@ -49,7 +38,7 @@ var server = http.createServer(function(request, response){
     response.end()
   }else{
     response.statusCode = 200
-    response.setHeader('Content-Type', 'text/css;charset=utf-8')
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write("输入的路径不存在对应的内容")
     response.end()
   }
